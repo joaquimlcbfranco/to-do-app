e5f0b523e3179fbee91c148025fb17b9d7205731
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-02-25 15:56:31 +0000

    Initial commit

---
+++ src/task.js
@@ -0,0 +1,14 @@
+const task = (() => {
+    class Task {
+        constructor(title, description, notes, priority, dueDate, complete) {
+            this.title = title;
+            this.description = description;
+            this.notes = notes;
+            this.priority = priority;
+            this.dueDate = dueDate;
+            this.complete = complete;
+        }
+    }
+
+    
+})();
