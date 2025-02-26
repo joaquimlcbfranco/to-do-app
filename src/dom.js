bbc6da69988e683163cd8b78d6e563c170d7a5fc
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-02-26 19:02:37 +0000

    Create loadForm and closeForm functions

---
+++ src/dom.js
@@ -0,0 +1,179 @@
+import projects from './tags.js'
+import tasks from './tasks.js'
+
+const dom = (() => {
+    const body = document.querySelector('body');
+    
+    const userDetails = document.querySelector('user-details');
+    
+    const navRows = document.querySelector('nav-buttons > nav-row');
+
+    const tags = document.querySelector('.tags');
+    const tagButton = document.querySelector('.tags > button');
+
+    const headerDetails = document.querySelector('.header-details');
+    const cardsContainer = document.querySelector('.cards');
+    const emptyCard = document.querySelector('.empty-card');
+
+    const fetchTasks = () => {
+        const tasks = document.querySelector('.card');
+        return tasks;
+    }
+
+
+    const loadForm = (projectIndex, taskIndex) => {
+        const dialog = document.createElement('dialog');
+        const wrapper = document.createElement('div');
+        const form = document.createElement('form');
+
+        const closeButton = document.createElement('button');
+
+        const titleLabel = document.createElement('label');
+        const titleSpan1 = document.createElement('span');
+        const titleText = document.createElement('p');
+        const titleSpan2 = document.createElement('span');
+        const formTitle = document.createElement('input');
+
+        const descriptionLabel = document.createElement('label');
+        const descriptionSpan1 = document.createElement('span');
+        const descriptionText = document.createElement('p');
+        const descriptionSpan2 = document.createElement('span');
+        const formDescription = document.createElement('input');
+
+        const notesLabel = document.createElement('label');
+        const notesSpan1 = document.createElement('span');
+        const notesText = document.createElement('p');
+        const notesSpan2 = document.createElement('span');
+        const formNotes = document.createElement('textarea');
+
+        const priorityLabel = document.createElement('label');
+        const prioritySpan1 = document.createElement('span');
+        const priorityText = document.createElement('p');
+        const prioritySpan2 = document.createElement('span');
+        const prioritySelect = document.createElement('select');
+        const none = document.createElement('option');
+        const lowPriority = document.createElement('option');
+        const mediumPriority = document.createElement('option');
+        const highPriority = document.createElement('option');
+        const criticalPriority = document.createElement('option');
+
+        const dateLabel = document.createElement('label');
+        const dateSpan1 = document.createElement('span');
+        const dateText = document.createElement('p');
+        const dateSpan2 = document.createElement('span');
+        const formDate = document.createElement('input');
+
+        const submitButton = document.createElement('button');
+
+        wrapper.classList.add('wrapper');
+
+        form.classList.add('dialog-form');
+
+        closeButton.classList.add('form-close');
+        closeButton.setAttribute('type', 'button');
+        closeButton.addEventListener('click', () => closeForm(dialog));
+
+        titleText.textContent = 'Title';
+        formTitle.id = 'form-title';
+        formTitle.type = 'text'
+
+        descriptionText.textContent = 'Description';
+        formDescription.id = 'form-description';
+        formDescription.type = 'text'
+
+        notesText.textContent = 'Notes';
+        formNotes.id = 'form-notes';
+
+        priorityText.textContent = 'Priority';
+        prioritySelect.id = 'form-select';
+        none.setAttribute('value', 'none');
+        none.setAttribute('selected', '');
+        none.textContent = '';
+        lowPriority.setAttribute('value', 'low');
+        lowPriority.textContent = 'Low';
+        mediumPriority.setAttribute('value', 'medium');
+        mediumPriority.textContent = 'Medium';
+        highPriority.setAttribute('value', 'high');
+        highPriority.textContent = 'High';
+        criticalPriority.setAttribute('value', 'critical');
+        criticalPriority.textContent = 'Critical';
+
+        dateText.textContent = 'Date';
+        formDate.id = 'form-date';
+        formDate.type = 'date';
+
+        submitButton.textContent = 'Submit';
+        submitButton.classList.add('form-button');
+        submitButton.setAttribute('type', 'submit');
+        submitButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            submitForm();
+        });
+
+        body.appendChild(dialog);
+        dialog.appendChild(wrapper);
+        wrapper.appendChild(form);
+
+        form.appendChild(closeButton);
+
+        form.appendChild(titleLabel);
+        titleLabel.appendChild(titleSpan1);
+        titleLabel.appendChild(titleText);
+        titleLabel.appendChild(titleSpan2);
+        form.appendChild(formTitle);
+
+        form.appendChild(descriptionLabel);
+        descriptionLabel.appendChild(descriptionSpan1);
+        descriptionLabel.appendChild(descriptionText);
+        descriptionLabel.appendChild(descriptionSpan2);
+        form.appendChild(formDescription);
+
+
+        form.appendChild(notesLabel);
+        notesLabel.appendChild(notesSpan1);
+        notesLabel.appendChild(notesText);
+        notesLabel.appendChild(notesSpan2);
+        form.appendChild(formNotes);
+
+        form.appendChild(priorityLabel);
+        priorityLabel.appendChild(prioritySpan1);
+        priorityLabel.appendChild(priorityText);
+        priorityLabel.appendChild(prioritySpan2);
+        form.appendChild(prioritySelect);
+        prioritySelect.appendChild(none);
+        prioritySelect.appendChild(lowPriority);
+        prioritySelect.appendChild(mediumPriority);
+        prioritySelect.appendChild(highPriority);
+        prioritySelect.appendChild(criticalPriority);
+
+        form.appendChild(dateLabel);
+        dateLabel.appendChild(dateSpan1);
+        dateLabel.appendChild(dateText);
+        dateLabel.appendChild(dateSpan2);
+        form.appendChild(formDate);
+
+        form.appendChild(submitButton);
+
+        dialog.showModal();
+    }
+
+    const closeForm = (dialog) => {
+        dialog.close();
+    }
+
+    const submitForm = () => {
+
+    }
+
+    emptyCard.addEventListener('click', () => {
+        loadForm();
+    });
+
+    return {
+        fetchTasks,
+        loadForm,
+        closeForm,
+    }
+})();
+
+export default dom
