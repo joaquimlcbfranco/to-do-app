951076e9efb0ab3fb33dba008ddb561e34994b77
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-02-27 13:46:12 +0000

    Import events.js events function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import tags from './tags.js'
 import tasks from './tasks.js'
 import dom from './dom.js'
+import events from "./events.js";
 
 window.tags = tags;
 window.tasks = tasks;
