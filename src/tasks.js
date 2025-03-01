f67a00ace3d052d975be9420da7c4bb2840177e6
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-03-01 19:50:44 +0000

    Remove description and priority from tasks objects

--- src/tasks.js
+++ src/tasks.js
@@ -2,27 +2,23 @@ import tags from "./tags.js";
 
 const tasks = (() => {
     class Task {
-        constructor(title, description, notes, priority, dueDate, complete) {
+        constructor(title, notes, dueDate, complete) {
             this.title = title;
-            this.description = description;
             this.notes = notes;
-            this.priority = priority;
             this.dueDate = dueDate;
             this.complete = complete;
         }
     }
 
-    const addTask = (tagIndex, title, description = '', notes = '', priority = '', dueDate, complete) => {
-        const task = new Task(title, description, notes, priority, dueDate, complete);
+    const addTask = (tagIndex, title, notes = '', dueDate, complete) => {
+        const task = new Task(title, notes, dueDate, complete);
         tags.tagList[tagIndex].tasks.push(task);
         console.log(tags.tagList[tagIndex]);
     };
 
-    const editTask = (tagIndex, taskIndex, title, description, notes, priority, dueDate) => {
+    const editTask = (tagIndex, taskIndex, title, notes, dueDate) => {
         tags.tagList[tagIndex].tasks[taskIndex].title = title;
-        tags.tagList[tagIndex].tasks[taskIndex].description = description;
         tags.tagList[tagIndex].tasks[taskIndex].notes = notes;
-        tags.tagList[tagIndex].tasks[taskIndex].priority = priority;
         tags.tagList[tagIndex].tasks[taskIndex].dueDate = dueDate;
     };
 
