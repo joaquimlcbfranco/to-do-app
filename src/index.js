78d5e11023d767c4a91e48bbc5d6b06820e3d0f3
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-03-04 15:24:30 +0000

    Clear up

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import "./styles.css";
-import tags from './tags.js'
-import tasks from './tasks.js'
-import dom from './dom.js'
-import events from './events.js'
+import tags from './tags.js';
+import tasks from './tasks.js';
+import dom from './dom.js';
+import events from './events.js';
 
 window.tags = tags;
 window.tasks = tasks;
 window.dom = dom;
-window.clear;
 
-tags.addTag('General','#576F72');
-tags.addTag('test1', '#FF0000');
-tags.addTag('test2', '#00FF00');
-tasks.addTask(0, 'today', '', '2025/03/03', false);
-// tasks.addTask(0, 'this week', '', '2025/03/05', false);
-// tasks.addTask(2, 'this week', '', '2025/03/07', false);
-// tasks.addTask(1, 'this month', '', '2025/03/20', false);
-// tasks.addTask(1, 'this month', '', '2025/03/21', false);
+tags.addTag('General', '#576F72');
 dom.displayTags();
 dom.displayTasks();
-dom.loadTasksForm();
