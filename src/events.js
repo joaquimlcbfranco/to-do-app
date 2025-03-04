62e6b2748e4c8cd79a9aa9c09406c29c3014e775
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-03-04 13:32:12 +0000

    Listen for task checkbox clicks and call markDone function

--- src/events.js
+++ src/events.js
@@ -28,17 +28,29 @@ const events = (() => {
         else if (e.target.classList.contains('tag-title')) {
             const tagIndex = e.target.parentNode.getAttribute('data-tag-id');
             tasks.filterTasks(tagIndex);
-            dom.highlightSelectedTag(e.target.parentNode);
+            dom.highlightElement(e.target.parentNode);
         }
-        else if (e.target.clasList.contains()) {
-            
+        else if (e.target.classList.contains('home')) {
+            tasks.filterTasks('home');
+            dom.highlightNav
+        }
+        else if (e.target.classList.contains('today')) {
+            tasks.filterTasks('other', 'today');
+        }
+        else if (e.target.classList.contains('week')) {
+            tasks.filterTasks('other', 'week');
+        }
+        else if (e.target.classList.contains('month')) {
+            tasks.filterTasks('other', 'month');
         }
         else if (e.target.classList.contains('tags-form-button')) {
             e.preventDefault();
             dom.submitTagsForm()
         }
-        else if (e.target.classList.contains('indicator') || e.target.classList.contains('form-checkbox')) {
-
+        else if (e.target.classList.contains('indicator') || e.target.classList.contains('checkbox-span')) {
+            const tagIndex = e.target.parentNode.parentNode.parentNode.getAttribute('data-tag-id');
+            const taskIndex = e.target.parentNode.parentNode.parentNode.getAttribute('data-task-id');
+            dom.markDone(e.target.parentNode.parentNode.parentNode, tagIndex, taskIndex);
         }
         else if (e.target.classList.contains('form-close')) {
             dom.closeForm();
