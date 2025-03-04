bfed2d19e63cde4dceb1106e9f46db7b145fdfe7
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-03-04 13:32:24 +0000

    Testing

--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@ import "./styles.css";
 import tags from './tags.js'
 import tasks from './tasks.js'
 import dom from './dom.js'
-import events from "./events.js";
+import events from './events.js'
 
 window.tags = tags;
 window.tasks = tasks;
 window.dom = dom;
 window.clear;
 
-tags.addTag('General','rgb(87, 111, 114)');
+tags.addTag('General','#576F72');
 tags.addTag('test1', '#FF0000');
 tags.addTag('test2', '#00FF00');
-tasks.addTask(0, 'title1', '', '2025/02/02', false);
-tasks.addTask(0, 'title2', '', '2025/03/04', false);
-tasks.addTask(1, 'title3', '', '2025/03/20', false);
-tasks.filterTasks(1);
+tasks.addTask(0, 'today', '', '2025/03/03', false);
+// tasks.addTask(0, 'this week', '', '2025/03/05', false);
+// tasks.addTask(2, 'this week', '', '2025/03/07', false);
+// tasks.addTask(1, 'this month', '', '2025/03/20', false);
+// tasks.addTask(1, 'this month', '', '2025/03/21', false);
 dom.displayTags();
+dom.displayTasks();
+dom.loadTasksForm();
