d50f5d0b6541ed23169b1819509d74e5bd584cac
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-03-04 13:33:30 +0000

    Fetch and add color to tag objects

--- src/tags.js
+++ src/tags.js
@@ -15,18 +15,15 @@ const tags = (() => {
     const addTag = (title, color) => {
         const tag = new Tag(title, color);
         tagList.push(tag);
-        console.log(tagList);
     }
 
     const editTag = (tagIndex, title, color) => {
         tagList[tagIndex].title = title;
         tagList[tagIndex].color = color;
-        console.log(tagList);
     }
 
     const deleteTag = (tagIndex) => {
         tagList.splice(tagIndex, 1);
-        console.log(tagList);
     }
 
     return {
